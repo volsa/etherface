2d6c504dbc5292cc3de819094a2e2ea6f2db8a51
Volkan <volkanxvs@gmail.com>
2022-09-07 11:23:05 +0000

    fix(rest): Disallow queries with page index < 1
    Fixes the "OFFSET must not be negative" error when sending a request
    such as `api.etherface.io/v1/signatures/text/all/balanceOf/-100`.
    Also deduplicate code, specficially the `Kind` to `SignatureKind` convertion.

--- etherface-rest/src/v1.rs
+++ etherface-rest/src/v1.rs
@@ -38,20 +38,33 @@ pub struct AppState {
     pub dbc: DatabaseClientPooled,
 }
 
+#[inline]
+fn is_valid_page_index(index: i64) -> bool {
+    index >= 1
+}
+
+#[inline]
+fn query_kind_to_signaturekind(kind: &Kind) -> Option<SignatureKind> {
+    match kind {
+        Kind::All => None,
+        Kind::Function => Some(SignatureKind::Function),
+        Kind::Event => Some(SignatureKind::Event),
+        Kind::Error => Some(SignatureKind::Error),
+    }
+}
+
 #[get("/signatures/text/{kind}/{input}/{page}")]
 async fn signatures_by_text(path: web::Path<ContentPath>, state: web::Data<AppState>) -> impl Responder {
+    if !is_valid_page_index(path.page) {
+        return HttpResponse::BadRequest().body("Page index must be >= 1");
+    }
+
     let input_trimmed = path.input.trim();
     if input_trimmed.len() < 3 {
         return HttpResponse::BadRequest().body("Query must have at least 3 characters");
     }
 
-    let kind = match path.kind {
-        Kind::All => None,
-        Kind::Function => Some(SignatureKind::Function),
-        Kind::Event => Some(SignatureKind::Event),
-        Kind::Error => Some(SignatureKind::Error),
-    };
-
+    let kind = query_kind_to_signaturekind(&path.kind);
     match state.dbc.rest().signatures_where_text_starts_with(&input_trimmed, kind, path.page) {
         Some(signatures) => HttpResponse::Ok().body(serde_json::to_string(&signatures).unwrap()),
         None => HttpResponse::NotFound().finish(),
@@ -60,6 +73,10 @@ async fn signatures_by_text(path: web::Path<ContentPath>, state: web::Data<AppSt
 
 #[get("/signatures/hash/{kind}/{input}/{page}")]
 async fn signatures_by_hash(path: web::Path<ContentPath>, state: web::Data<AppState>) -> impl Responder {
+    if !is_valid_page_index(path.page) {
+        return HttpResponse::BadRequest().body("Page index must be >= 1");
+    }
+
     let mut input_trimmed = path.input.trim();
     if input_trimmed.starts_with("0x") {
         input_trimmed = &input_trimmed[2..];
@@ -69,13 +86,7 @@ async fn signatures_by_hash(path: web::Path<ContentPath>, state: web::Data<AppSt
         return HttpResponse::BadRequest().body("Query must have 8 or 64 characters");
     }
 
-    let kind = match path.kind {
-        Kind::All => None,
-        Kind::Function => Some(SignatureKind::Function),
-        Kind::Event => Some(SignatureKind::Event),
-        Kind::Error => Some(SignatureKind::Error),
-    };
-
+    let kind = query_kind_to_signaturekind(&path.kind);
     match state.dbc.rest().signature_where_hash_starts_with(&input_trimmed, kind, path.page) {
         Some(signatures) => HttpResponse::Ok().body(serde_json::to_string(&signatures).unwrap()),
         None => HttpResponse::NotFound().finish(),
@@ -84,13 +95,11 @@ async fn signatures_by_hash(path: web::Path<ContentPath>, state: web::Data<AppSt
 
 #[get("/sources/github/{kind}/{signature_id}/{page}")]
 async fn sources_github(path: web::Path<SourcePath>, state: web::Data<AppState>) -> impl Responder {
-    let kind = match path.kind {
-        Kind::All => None,
-        Kind::Function => Some(SignatureKind::Function),
-        Kind::Event => Some(SignatureKind::Event),
-        Kind::Error => Some(SignatureKind::Error),
-    };
+    if !is_valid_page_index(path.page) {
+        return HttpResponse::BadRequest().body("Page index must be >= 1");
+    }
 
+    let kind = query_kind_to_signaturekind(&path.kind);
     match state.dbc.rest().sources_github(path.signature_id, kind, path.page) {
         Some(signatures) => HttpResponse::Ok().body(serde_json::to_string(&signatures).unwrap()),
         None => HttpResponse::NotFound().finish(),
@@ -99,13 +108,11 @@ async fn sources_github(path: web::Path<SourcePath>, state: web::Data<AppState>)
 
 #[get("/sources/etherscan/{kind}/{signature_id}/{page}")]
 async fn sources_etherscan(path: web::Path<SourcePath>, state: web::Data<AppState>) -> impl Responder {
-    let kind = match path.kind {
-        Kind::All => None,
-        Kind::Function => Some(SignatureKind::Function),
-        Kind::Event => Some(SignatureKind::Event),
-        Kind::Error => Some(SignatureKind::Error),
-    };
+    if !is_valid_page_index(path.page) {
+        return HttpResponse::BadRequest().body("Page index must be >= 1");
+    }
 
+    let kind = query_kind_to_signaturekind(&path.kind);
     match state.dbc.rest().sources_etherscan(path.signature_id, kind, path.page) {
         Some(signatures) => HttpResponse::Ok().body(serde_json::to_string(&signatures).unwrap()),
         None => HttpResponse::NotFound().finish(),
